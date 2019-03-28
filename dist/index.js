4410ee72812c0098b5ffcc99ac42d14899fbe4fd
John Wehr <johnwehr@gmail.com>
2019-03-28 15:29:03 -0400

    Thumbnail URL getter

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,4 @@
 export { default as BlendClient } from './client';
 export { default as makeBlendLogger } from './logger';
 export { default as blendServerDetectedPromise } from './server-detection';
+export { default as getBlendThumbnailUrl } from './thumbnail-url';
---
+++ dist/thumbnail-url.js
@@ -0,0 +1,11 @@
+//      
+
+import blendServerDetectedPromise from './server-detection';
+
+export default async (streamUrl       ) => {
+  const blendServerDetected = await blendServerDetectedPromise;
+  if (!blendServerDetected) {
+    throw new Error(`Unable to generate thumbnail, Blend Server not detected`);
+  }
+  return `http://127.0.0.1:61340/api/1.0/stream/${encodeURIComponent(streamUrl)}/thumbnail.jpg`;
+}
