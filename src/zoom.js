d7b5e8828bd3bc777ef542f08458f0c48874b625
Adam Wang <adamw3494@gmail.com>
2019-07-24 17:54:58 -0400

    Additional routes

--- src/zoom.js
+++ src/zoom.js
@@ -7,7 +7,7 @@ const BASE_API_URL = 'http://127.0.0.1:61340/api/1.0/zoom';
 async function post(path: string, data: Object = {}) {
   try {
     return superagent
-      .post(`${BASE_API_URL}/${path}`)
+      .post(`${BASE_API_URL}${path}`)
       .send(data);
   } catch (error) {
     const { response: { text } } = error;
@@ -16,12 +16,34 @@ async function post(path: string, data: Object = {}) {
 }
 
 async function join(meetingNumber: string, password: string) {
-  await post('join', { meetingNumber, password });
+  await post('/join', { meetingNumber, password });
 }
 
 async function leave() {
-  await post('leave');
+  await post('/leave');
 }
+
+async function volume(volume: number) {
+  await post('/audio/volume', { volume });
+}
+
+async function muteMic() {
+  await post('/mic/mute');
+}
+
+async function unmuteMic() {
+  await post('/mic/unmute');
+}
+
+async function enableVideo() {
+  await post('/video/enable');
+}
+
+async function disableVideo() {
+  await post('/video/disable');
+}
+
+
 
 const zoom = {
   join,
